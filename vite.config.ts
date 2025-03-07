dcbb9e7858794ade11b06e63b78156296b000744
Carey Spies <carey.spies@gmail.com>
2025-03-06 20:03:16 -0800

    update for gh docs

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ds-configurator/',
   build: {
     outDir: 'docs',
     emptyOutDir: true
